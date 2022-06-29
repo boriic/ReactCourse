06f7b70aa6da74b0e9dda2e90528945de4bb209a
Filip <filipmcsg@gmail.com>
2022-06-29 22:15:39 +0200

    learning basics of typescript

---
+++ typescript/basics.ts
@@ -0,0 +1,3 @@
+let age:number = 2;
+let string:string = "whaa";
+let isInstructor:boolean = true;
---
+++ typescript/no-typescript.js
@@ -0,0 +1,7 @@
+function add1(a, b) {
+  return a + b;
+}
+
+const result1 = add(2, 5);
+
+console.log(result1);
